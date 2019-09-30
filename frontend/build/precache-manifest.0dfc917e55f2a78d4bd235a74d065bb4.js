b453a807a0297b37dcbe0d7a06dc5e3e5ce8a834
reynoldjong <55976052+reynoldjong@users.noreply.github.com>
2019-09-30 04:32:30 -0400

    Add files via upload

---
+++ frontend/build/precache-manifest.0dfc917e55f2a78d4bd235a74d065bb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7706ba0b0b4295112ee460d22c8ec54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d903d4b45066584a52",
+    "url": "/static/css/main.82ff57d4.chunk.css"
+  },
+  {
+    "revision": "7e93db1d8103d1699712",
+    "url": "/static/js/2.75bb4c4e.chunk.js"
+  },
+  {
+    "revision": "21d903d4b45066584a52",
+    "url": "/static/js/main.bfd37a4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "742bc46a5fac956aaf8b74466e904150",
+    "url": "/static/media/DFILogo.742bc46a.png"
+  },
+  {
+    "revision": "92e9724a1071b7b25b7449fc65f3ee47",
+    "url": "/static/media/analytics.92e9724a.png"
+  },
+  {
+    "revision": "28c46de0cc3cbf8baf36e28256ef9ee5",
+    "url": "/static/media/chatbot.28c46de0.png"
+  },
+  {
+    "revision": "6b617478413025ae5808d3aeea11b6c8",
+    "url": "/static/media/robot.6b617478.png"
+  }
+]);
